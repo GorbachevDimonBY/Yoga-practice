89fef01deabc65bae7a206e0330060ef8cc3be2f
Dmitry Gorbacev <GorbachevDimon395@gmail.com>
2020-01-03 00:22:35 +0300

    first commit

---
+++ src/js/parts/modal.js
@@ -0,0 +1,28 @@
+function modal() {
+  let more = document.querySelector('.more'),
+  overlay = document.querySelector('.overlay'),
+  close = document.querySelector('.popup-close'),
+  moreInTab = document.querySelectorAll('.description-btn');
+
+  more.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  });
+
+  moreInTab.forEach(tab => {
+    tab.addEventListener('click', function() {
+      overlay.style.display = 'block';
+      this.classList.add('more-splash');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  close.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  });
+}
+
+module.exports = modal;
